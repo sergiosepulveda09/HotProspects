6e4b4ba58510535f540a8bbc3cedd5ff8e6e539f
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-08-19 23:27:31 -0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Sergio Sepulveda on 2021-08-19.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
